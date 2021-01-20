cb631805e21e7e50d01566814693d5010717cf93
Rodel Arenas <rodel.arenas@digify.com.ph>
2021-01-20 21:13:43 +0800

    Modify requirements.txt

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: sh setup.sh && streamlit run hello-world
-worker: YOUR_COMMAND
+web: sh setup.sh && streamlit run index.py
--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 mkdir -p ~/.streamlit/
 
-echo “
-[general]
-email = \”rodelarenas@gmail.com\”
-“ > ~/.streamlit/credentials.toml
+echo "\
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
 
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
